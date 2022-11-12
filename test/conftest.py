a110624f13241b7617504be97732740377744664
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-11-12 11:06:42 +0100

    Fix conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -3,23 +3,6 @@ Setup for pytest.
 """
 
 import pytest
-import torch
+import numpy as np
 
-torch.set_printoptions(precision=10)
-
-
-def pytest_addoption(parser: pytest.Parser):
-    """Set up additional command line options."""
-
-    parser.addoption(
-        "--detect-anomaly",
-        action="store_true",
-        help="Enable more comprehensive gradient tests.",
-    )
-
-
-def pytest_configure(config: pytest.Config):
-    """Pytest configuration hook."""
-
-    if config.getoption("--detect-anomaly"):
-        torch.autograd.anomaly_mode.set_detect_anomaly(True)
+np.set_printoptions(precision=10)
