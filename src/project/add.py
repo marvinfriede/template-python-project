fd19971629598845db15608f5f47a5beeeebe1f5
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-11-12 11:54:58 +0100

    Fix type

--- src/project/add.py
+++ src/project/add.py
@@ -1,5 +1,5 @@
 def add_floats(a: float, b: float) -> float:
-  if not all(isinstance(i, int) for i in [a, b]):
+  if not all(isinstance(i, float) for i in [a, b]):
     raise TypeError("Only floats can be passed.")
 
   return a + b
