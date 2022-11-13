02f1e9ade7fdb3e29fd31ab762851b06990fbdf0
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-11-13 12:14:28 +0100

    Format

--- src/squarer/__init__.py
+++ src/squarer/__init__.py
@@ -4,24 +4,19 @@ Entry point for command line interface.
 
 import argparse
 import sys
-from .maths import square_a_number as square
+
 from .__version__ import __version__
+from .maths import square_a_number as square
+
 
 def main() -> None:
-    
     # get command line argument
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "number",
-        type=float,
-        help="Number to square."
-    )
+    parser.add_argument("number", type=float, help="Number to square.")
     args = parser.parse_args()
-    
-    
+
     print(square(args.number))
-    
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
--- test/test_math/test_square.py
+++ test/test_math/test_square.py
@@ -1,6 +1,7 @@
 import pytest
+
 from squarer.maths import square_a_number
 
 
 def test_add() -> None:
-  assert pytest.approx(square_a_number(2.0)) == 4.0
+    assert pytest.approx(square_a_number(2.0)) == 4.0
